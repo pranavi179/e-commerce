2828d0f1e97e37594858933474af017ed6f8f7a1
pranavi179 <pranavivelisoju@gmail.com>
2019-11-06 15:20:51 +0530

    files added

---
+++ ecomm/src/component/register.js
@@ -0,0 +1,14 @@
+import React , { Component } from 'react';
+import logo from './logo.svg';
+import './styles/register.css';
+
+
+export default class register extends React.Component{
+    render() {
+        return(
+            <nav className = "register" >
+                
+            </nav>
+        )
+    }
+}
