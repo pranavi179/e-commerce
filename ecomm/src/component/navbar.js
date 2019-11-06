f46ef37286f9475547e6894d2c17a77f49760f52
pranavi179 <pranavivelisoju@gmail.com>
2019-11-06 22:32:30 +0530

    editing register page

---
+++ ecomm/src/component/navbar.js
@@ -0,0 +1,15 @@
+import React , { Component } from 'react';
+import '../App.css'; 
+class Navbar extends Component {
+    render(){
+    return(       
+        <nav>
+<h2>Navbar</h2>
+        </nav>
+        
+      </div>
+               
+    );
+    }
+}
+export default Navbar;
--- ecomm/src/component/register.js
+++ ecomm/src/component/register.js
@@ -1,5 +1,6 @@
 import React , { Component } from 'react';
 // import Modal from './component/modal.js';
+import '../App.css'; 
 
 
 
@@ -10,11 +11,26 @@ export default class Register extends Component{
     }
     render() {
         return(
+            // <Navbar />
             <div>
                 {/* <Modal> */}
-                    <div class = "container">
-                    <h1>erfsdf</h1>
-                    </div>
+                <div class="card-panel teal lighten-2">
+                    {/* <div class = "container"> */}
+                    <h1>Register</h1></div>
+                    <ul id="tabs-swipe-demo" class="tabs">
+                    <li class="tab col s3"><a href="#test-swipe-1">Register</a></li>
+                    <li class="tab col s3"><a class="active" href="#test-swipe-2">Login</a></li>
+  
+  </ul>
+  <div id="test-swipe-1" class="col s12 blue">Register</div>
+  <div id="test-swipe-2" class="col s12 red">Login</div>
+                  
+
+
+                    {/* </div> */}
+                   
+            
+
                 
                 {/* </Modal> */}
             </div>
