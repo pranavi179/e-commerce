38e429dde5e892332d4dbf83d6b7a079f944ab0d
BigDaddy-Zephyr <54617208+BigDaddy-Zephyr@users.noreply.github.com>
2019-11-08 15:42:13 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var crypto = require('crypto');
-var bodyParser = require('body-parser');
-
+var bodyParser = require('body-parser'), bcrypt = require('bcrypt');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
@@ -63,30 +62,96 @@ app.use("/signin", (req, res) => {
 });
 
 
+// userSchema.body-parser('save', function(next) {
+//     var user = this;
+//     console.log("///////");
+//     // only hash the password if it has been modified (or is new)
+//     if (!user.isModified('password')) return next();
+
+//     // generate a salt
+//     bcrypt.genSalt(function(err, salt) {
+//         if (err) return next(err);
+
+//         // hash the password along with our new salt
+//         bcrypt.hash(user.password, salt, function(err, hash) {
+//             if (err) return next(err);
+
+//             // override the cleartext password with the hashed one
+//             console.log(hash);
+//             user.password = hash;
+//             next();
+//         });
+//     });
+// });
+
+
+
+
+
+
 app.use("/signup", (req, res) => {
  res.render(__dirname + "/views/signup.ejs");
 });
 
-app.post("/adduser", (req, res) => {
-  var data = req.body;
-userSchema.methods.setPassword = function(password){
-  this.salt = crypto.randomBytes(16).toString('hex');
-  this.hash = crypto.pbkdf2Sync(data.password, this.salt, 1000, 64, 'sha512').toString('hex');
-};
+app.post("/login", (req,res) =>{
 
+  console.log("heloooooo");
+let data= req.body;
+console.log(data.username);
+var user_n= User.findOne({ username: data.username },function (err, doc){
+ console.log(doc.password);
+ // user_n= doc.password;
+// localstorage.setItem(token:"hashvalue");
 
-userSchema.methods.setPassword();
-console.log("///////////");
- var myData = new User(req.body);
- myData.save()
- .then(item => {
- res.send("item saved to database");
- })
- .catch(err => {
- res.status(400).send("unable to save to database");
- });
+console.log("///////////",doc.password);
+        // test a matching password
+    bcrypt.compare(doc.password, data.password, function(err, isMatch) {
+          if (err) {console.log("error",err);}
+          else{
+            console.log("Match");
+            res.send("Signed in");
+
+          }
+    });
+
+});
+ 
 });
 
+
+
+
+
+
+app.post("/adduser", (req, res) => {
+  let data = req.body;
+
+   bcrypt.genSalt(function(err, salt) {
+      if (err){
+        console.log("ERRRRRRRRRRRRRRRRRRRRor");
+      };
+
+      // hash the password along with our new salt
+      bcrypt.hash(data.password, salt, function(err, hash) {
+          if (err) {
+            console.log("Error in hashing")
+          };
+
+          // override the cleartext password with the hashed one
+          console.log("Hash", hash);
+          data.password = hash;
+          console.log("DAta", data)
+         var myData = new User(data);
+         myData.save()
+         .then(item => {
+         res.send("item saved to database");
+         })
+         .catch(err => {
+         res.status(400).send("unable to save to database");
+         });
+      });
+  });
+ });
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -114,3 +179,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+
