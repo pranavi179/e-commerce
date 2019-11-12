6bed3eb1482c538648ef6254dccf7c6c8abdf4ca
BigDaddy-Zephyr <anujduggal61@gmail.com>
2019-11-12 16:52:05 +0530

    made changes

--- app.js
+++ app.js
@@ -224,7 +224,19 @@ Product.findOne().sort({$natural: -1}).limit(1).exec(function(err, doc){
 // res.send(doc.price);
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
+});
 // })
 
 // })
