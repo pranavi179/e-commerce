ba4c92aab1d66d5ac937f33cec8e4e2156c95727
madhu-kuncham <kmadhukumar88011@gmail.com>
2019-11-12 10:58:13 +0530

    changes made in view

--- src/components/view.js
+++ src/components/view.js
@@ -27,6 +27,7 @@ export default class View extends React.Component {
         <Breadcrumb.Item>Home</Breadcrumb.Item>
         <Breadcrumb.Item>List</Breadcrumb.Item>
         <Breadcrumb.Item>App</Breadcrumb.Item>
+        <Breadcrumb.Item>productItem</Breadcrumb.Item>
       </Breadcrumb>
       <Layout style={{ padding: '24px 0', background: '#fff' }}>
         <Sider width={200} style={{ background: '#fff' }}>
